548eb1683d668b8e8df8bfd187aedde5088cd06c
youngmikec <42143597+youngmikec@users.noreply.github.com>
2020-06-17 16:08:38 +0100

    Add files via upload

---
+++ buildScripts/message.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+let message = "Starting app in Dev mode"
+console.log(chalk.blue(message));
---
+++ buildScripts/srcServer.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+const app = express();
+
+const port = 3001;
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+    })
+);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        open('http://localhost:' + port);
+    }
+})
