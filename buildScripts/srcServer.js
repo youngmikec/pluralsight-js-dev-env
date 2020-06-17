ddbd6626d65de98b99ca7e5a6916ba5c0dbbc991
ozor michael chisom <michaelozor15@gmail.com>
2020-06-17 14:45:54 +0100

    I updated my node modules and used ngrok instead of localtunnel for sharing

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,5 +1,29 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3001;
+const app = express();
+
+const port = 3001;
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+    })
+);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        open('http://localhost:' + port);
+    }
+})
